a5cdd8015919b50fefe6391a32f80711c41fe30d
bb <738508231@qq.com>
2024-05-13 21:06:20 +0800

    Initial Commit

---
+++ gittest1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gittest1
+//
+//  Created by wjc on 2024/5/13.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
